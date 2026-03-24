8c84d0966b0a629c06fb46ae341b4b3eae05276c
Michael Ward <misha.r.ward@gmail.com>
2026-03-24 06:49:26 +0000

    Minor updates to the Two Sum problem

--- java/src/problems/arrays/two_sum_0001/Solution.java
+++ java/src/problems/arrays/two_sum_0001/Solution.java
@@ -3,8 +3,22 @@ package arrays.two_sum_0001;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+    Problem:
+    Given an array of integers, return indices of the two numbers such that they add up to a target.
+
+    Approach:
+    - Use a hash map to store values and their indices
+    - For each element, check if its complement exists in the map
+    - Return the indices of the two elements that sum to the target
+
+    Time: O(n)
+    Space: O(n)
+*/
+
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // assume exactly one solution exists as per problem constraints
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
