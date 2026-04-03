f20c6c3a3b39cf5f1d9341b3b78c98935d5cf9ca
Xiaoqi Jiang <kjad123@yahoo.com>
2026-04-03 12:27:04 -0500

    Add the solution for problem 2520

---
+++ python/problems/arrays/count_digits_2520/solution.py
@@ -0,0 +1,15 @@
+def countDigits(self, num: int) -> int:
+    count = 0
+
+    # create a hash table - does not allow duplicate
+    num_s = str(num)
+    val = []
+
+    for i in range(len(num_s)):
+        val.append(num_s[i])
+
+    for v in val:
+        if num % int(v) == 0:
+            count += 1
+
+    return count
