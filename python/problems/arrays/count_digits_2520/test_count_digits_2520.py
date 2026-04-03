73b08e1e23160b0cc6b8f48a670228d90c08c51f
Xiaoqi Jiang <kjad123@yahoo.com>
2026-04-03 12:27:15 -0500

    Add the unit test for problem 2520

---
+++ python/problems/arrays/count_digits_2520/test_count_digits_2520.py
@@ -0,0 +1,10 @@
+from python.problems.arrays.count_digits_2520.solution import countDigits
+
+def test_example_1():
+    assert countDigits(7) == 1
+
+def test_example_2():
+    assert countDigits(121) == 2
+
+def test_example_3():
+    assert countDigits(1248) == 4
